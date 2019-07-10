8d3bddac8682b657c1867e39bce97b6e96500bd2
ItoSchum <itoschum@gmail.com>
2019-07-10 19:10:18 +0800

    Rewrite Q1 with TC&SC

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -1,7 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-    	list_len = len(nums)
-    	for i in range(0, list_len - 1):
-    		for j in range(i + 1, list_len):
-    			if target == nums[i] + nums[j]:
-    				return [i, j]
+        nums_len = len(nums)
+        num_map = []
+        
+        for i in range(nums_len):
+            num_map.append(nums[i])
+
+        for i in range(nums_len):
+            complement = target - nums[i]
+            if complement in num_map and num_map.index(complement) != i:
+                return [i, num_map.index(complement)]
+        
+        return "No such solution"
+
+# Runtime: 780 ms, faster than 36.35% of Python3 online submissions for Two Sum.
+# Memory Usage: 13.5 MB, less than 97.58% of Python3 online submissions for Two Sum.
+
+# TC: O(n), n = nums_len
+# SC: O(n), n = size of num_map

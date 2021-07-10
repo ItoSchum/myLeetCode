710e5689df87907ab42eb8df29cc36c75876cd00
ItoSchum <itoschum@gmail.com>
2021-07-09 21:18:50 -0400

    Completed Medium 1038, 450, 701 (Python)

---
+++ 450.delete-node-in-a-bst.py
@@ -0,0 +1,106 @@
+#
+# @lc app=leetcode id=450 lang=python3
+#
+# [450] Delete Node in a BST
+#
+# https://leetcode.com/problems/delete-node-in-a-bst/description/
+#
+# algorithms
+# Medium (45.92%)
+# Likes:    3271
+# Dislikes: 114
+# Total Accepted:    182.6K
+# Total Submissions: 395.9K
+# Testcase Example:  '[5,3,6,2,4,null,7]\n3'
+#
+# Given a root node reference of a BST and a key, delete the node with the
+# given key in the BST. Return the root node reference (possibly updated) of
+# the BST.
+# 
+# Basically, the deletion can be divided into two stages:
+# 
+# 
+# Search for a node to remove.
+# If the node is found, delete the node.
+# 
+# 
+# Follow up: Can you solve it with time complexity O(height of tree)?
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: root = [5,3,6,2,4,null,7], key = 3
+# Output: [5,4,6,2,null,null,7]
+# Explanation: Given key to delete is 3. So we find the node with value 3 and
+# delete it.
+# One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+# Please notice that another valid answer is [5,2,6,null,4,null,7] and it's
+# also accepted.
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: root = [5,3,6,2,4,null,7], key = 0
+# Output: [5,3,6,2,4,null,7]
+# Explanation: The tree does not contain a node with value = 0.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: root = [], key = 0
+# Output: []
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in the tree is in the range [0, 10^4].
+# -10^5 <= Node.val <= 10^5
+# Each node has a unique value.
+# root is a valid binary search tree.
+# -10^5 <= key <= 10^5
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if root == None:
+            return None
+        
+        if key == root.val:
+            if root.left == None:
+                return root.right
+            if root.right == None:
+                return root.left
+            minNode = self.getMinLeftNode(root.right)        
+            root.val = minNode.val
+            root.right = self.deleteNode(root.right, minNode.val)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        return root
+            
+    def getMinLeftNode(self, root: TreeNode) -> TreeNode:
+        while root.left != None:
+            root = root.left
+        return root
+# @lc code=end
+
+# n = node number
+# Time: O(log2(n))
+# Space: O(1)
